193f05f2c9c071a3de00f8b207374f8605be2fd7
Nicholas Wenzel <nwenzel@mines.edu>
2019-03-05 11:56:47 -0700

    Initial Commit

---
+++ src/clueGame/Board.java
@@ -0,0 +1,51 @@
+package clueGame;
+
+import java.util.Set;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+public class Board {
+	private static final int MAX_BOARD_SIZE = 100; 
+	private int numRows;
+	private int numColumns; 
+	
+	public BoardCell[][] board; 
+	
+	public Map<Character, String> legend;
+	
+	public Map<BoardCell, Set<BoardCell>> adjMatrix; 
+	
+	public Set<BoardCell> targets; 
+	
+	public String boardConfigFile;
+	
+	public String roomConfigFile;
+	
+	
+	public Board getInstance() {
+		
+		return null; 
+	}
+	
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadRoomConfig() {
+		
+	}
+	
+	public void loadBoardConfig() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void calcTargets(BoardCell cell, int pathLength) {
+		
+	}
+	
+}
---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,16 @@
+package clueGame;
+
+public class BoardCell {
+	public int row;
+	
+	public int column;
+
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	
+	
+}
