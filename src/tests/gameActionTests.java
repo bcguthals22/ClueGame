eab284eea3b533c6e3da6c5b44b2ff5048c75afc
Brennan Guthals <bcguthals@mymail.mines.edu>
2019-03-29 18:13:54 -0600

    Added gameActionTests class

---
+++ src/tests/gameActionTests.java
@@ -0,0 +1,30 @@
+/*
+ * Brennan Guthals & Nicholas Wenzel
+ * 3/6/2019
+ * ClueGame
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.HumanPlayer;
+import clueGame.ComputerPlayer;
+
+
+public class gameActionTests {
+
+	@BeforeClass
+	public static void setUp() {
+		
+	}
+}
