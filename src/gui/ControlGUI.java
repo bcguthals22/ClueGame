1f208957149a3ea51978b88bf1c9092c61e46abb
Nicholas Wenzel <nwenzel@mymail.mined.edu>
2019-04-06 10:59:26 -0600

    Added WhoseTurn box and dieRollbox

--- src/gui/ControlGUI.java
+++ src/gui/ControlGUI.java
@@ -4,12 +4,19 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class ControlGUI extends JFrame {
 
@@ -25,8 +32,11 @@ public class ControlGUI extends JFrame {
 		JPanel panel = new JPanel();
 		
 		panel = addControlButtons();
-		
 		add(panel);
+		panel = whoseTurnBox();
+		add(panel, BorderLayout.WEST);
+		panel = dieRollBox();
+		add(panel, BorderLayout.SOUTH);
 		
 
 		
@@ -48,6 +58,46 @@ public class ControlGUI extends JFrame {
 		panel.add(makeAccu);
 		
 		return panel;
+	}
+	
+	
+	public JPanel whoseTurnBox() {
+		
+		JPanel panel = new JPanel();
+		
+		JTextField playerTurn;
+		JLabel label = new JLabel("Whose turn?");
+		
+		Border border = BorderFactory.createLineBorder(Color.black);
+		
+		playerTurn = new JTextField(20);
+		
+		playerTurn.setEditable(false);
+		
+		playerTurn.setBorder(border);
+		
+		panel.add(label);
+		panel.add(playerTurn);
+		
+		return panel; 
+		
+	}
+	
+	public JPanel dieRollBox() {
+		JPanel panel = new JPanel();
+
+		JTextField rollNum;
+		JLabel label = new JLabel("Roll");
+
+		rollNum = new JTextField(5);
+
+		rollNum.setEditable(false);
+
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Die"));
+
+		panel.add(rollNum);
+		
+		return panel; 
 	}
 
 
